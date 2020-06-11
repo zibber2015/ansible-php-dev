b77cf66665ef6e9d7721b31b7c8b9153f318049c
allen <zibber@qq.com>
2020-06-11 22:05:50 +0800

    Update install-ansible.sh

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt install software-properties-common -y && apt-add-repository ppa:ansible/ansible && apt update &&  && apt-get install ansible -y
+apt install software-properties-common -y && apt-add-repository ppa:ansible/ansible && apt update && apt-get install ansible -y
